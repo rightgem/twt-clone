2f89786bd460858e09a3f38c81ce1ab6f7af0686
rightgem <all_almost@naver.com>
2022-01-13 16:31:35 +0900

    set firebase 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import AppRouter from "components/Router";
-import fbase from "fbase"
+import authService from "fbase"
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false); //isLoggedIn 기본값 false
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser); //isLoggedIn 기본값 false
   return (
     <>
       <AppRouter isLoggedIn={isLoggedIn} />
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,4 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Auth = () => { return ( <h1>Auth</h1> )};
+const Auth = () => { 
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    
+    const onChange = (event) => {
+        const{ 
+            target : {name, value}, 
+        }= event;
+        if(name === "email"){
+            setEmail(value);
+        } else if (name === "pw"){
+            setPassword(value);
+        }
+    }
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+    }
+
+    return ( <div>
+        <form onSubmit={onSubmit}>
+            <input 
+                type="text"
+                name="email"
+                placeholder="Email" 
+                required 
+                value={email}
+                onChange={onChange} 
+            />
+            <input 
+                type="password"
+                name="pw" 
+                placeholder="Password" 
+                required 
+                value={password} 
+                onChange={onChange}
+            />
+            <input type="submit" value="LogIn" />
+        </form>
+        <div>
+            <button>Continue with Google</button>
+            <button>Continue with Github</button>
+        </div>
+    </div> )};
 export default Auth;
